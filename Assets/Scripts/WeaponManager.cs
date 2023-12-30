d7bdd2c3a31b1c1da57e04011dee930c9acad779
diliupg <diliupg@gmail.com>
2023-12-30 19:06:09 +0530

    pickup weapon and parent to player OK.

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
     public static WeaponManager Instance {get; set;}
+
+    public List<GameObject> weaponSlots;
+    public GameObject activeWeaponSlot;
 
     private void Awake()
     {
@@ -18,8 +22,38 @@ public class WeaponManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        activeWeaponSlot = weaponSlots[0];
+    }
+
+    private void Update()
+    {
+        foreach(GameObject weaponSlot in weaponSlots)
+        {
+            if(weaponSlot == activeWeaponSlot)
+            {
+                weaponSlot.SetActive(true);
+            }
+            else{
+                weaponSlot.SetActive(false);
+            }
+        }
+    }
     public void PickupWeapon(GameObject pickedUpWeapon)
     {
-        Destroy(pickedUpWeapon);
+        AddWeaponIntoActiveSlot(pickedUpWeapon);
+    }
+
+    private void AddWeaponIntoActiveSlot(GameObject pickedupWeapon)
+    {
+        pickedupWeapon.transform.SetParent(activeWeaponSlot.transform, false);
+
+        Weapon weapon = pickedupWeapon.GetComponent<Weapon>();
+
+        pickedupWeapon.transform.localPosition = new Vector3(weapon.spawnPosition.x, weapon.spawnPosition.y, weapon.spawnPosition.z);
+        pickedupWeapon.transform.localRotation = Quaternion.Euler(weapon.spawnRotation.x, weapon.spawnRotation.y, weapon.spawnRotation.z);
+
+        weapon.isActiveWeapon = true;
     }
 }
